ec4a5c6fc1a4077322b0d948549c5ac4b278d298
Thareek Anvar <thareekanvar@gmail.com>
2020-07-31 23:55:50 +0530

    firebase configre remove

--- components/firebase/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-  apiKey: "AIzaSyC5ZOOC_KDC8vqO3JoVatIK2bECW77KoYM",
-  authDomain: "expo-firebase-phone-starter.firebaseapp.com",
-  databaseURL: "https://expo-firebase-phone-starter.firebaseio.com",
-  projectId: "expo-firebase-phone-starter",
-  storageBucket: "expo-firebase-phone-starter.appspot.com",
-  messagingSenderId: "694084349503",
-  appId: "1:694084349503:web:e48744ee5ab40f6b06d28a",
-};
