e6e1d863f7d599f654043013c321974d7efd1977
Thareek Anvar <thareekanvar@gmail.com>
2020-07-31 21:41:50 +0530

    firebase Initial

---
+++ components/Firebase/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "XXXX",
+  authDomain: "XXXX",
+  databaseURL: "XXXX",
+  projectId: "XXXX",
+  storageBucket: "XXXX",
+  messagingSenderId: "XXXX",
+  appId: "XXXX",
+};
